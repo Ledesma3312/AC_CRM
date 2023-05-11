02d67fc471ea07c64a5bb3c0c1396b864f4ebc17
root <root@DESKTOP-MF63V4V>
2023-05-11 16:19:24 -0400

    Initial commit.

---
+++ src/activities_calendar_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { activities_calendar_backend } from "../../declarations/activities_calendar_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await activities_calendar_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
